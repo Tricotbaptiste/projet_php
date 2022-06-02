ef4ebd8db09760e3eb778805b58060f348c0035f
Tricotbaptiste <94443465+Tricotbaptiste@users.noreply.github.com>
2022-06-02 10:34:43 +0200

    Add files via upload

---
+++ quarantaine_vrac/main.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body style="margin:0">
+
+<h1>Main</h1>
+
+<img src="lol_logo.png">
+</body>
+</html>
---
+++ quarantaine_vrac/styl.php
@@ -0,0 +1,73 @@
+
+<?php
+/*** set the content type header ***/
+/*** Without this header, it wont work ***/
+header("Content-type: text/css");
+?>
+
+td { 
+    border: 0.5px solid
+ };
+
+
+div {
+
+margin: right;
+width:100 px
+}
+
+
+
+.center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+  };
+
+
+
+  .dropbtn {
+    background-color: #04AA6D;
+    color: white;
+    padding: 16px;
+    font-size: 16px;
+    border: none;
+  }
+  
+  .dropdown {
+    position: relative;
+    display: inline-block;
+    margin: 0 auto;
+  }
+  
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f1f1f1;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+  }
+  
+  .dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+  }
+  
+  .dropdown-content a:hover {background-color: #ddd;}
+  
+  .dropdown:hover .dropdown-content {display: block;}
+  
+  .dropdown:hover .dropbtn {background-color: #fa00e5;}
+
+
+  .box {
+    background-color: #04AA6D;
+  }
+
+a {
+  color:#3e8e41;
+}
